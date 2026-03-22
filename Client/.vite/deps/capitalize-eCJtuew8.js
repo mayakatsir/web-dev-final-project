5d138817fea704dd171c7797303173be2e9b0862
karensproject <karenitz2708@gmail.com>
2026-03-22 17:35:42 +0200

    edit .gitignore

--- Client/.vite/deps/capitalize-eCJtuew8.js
+++
@@ -1,7 +0,0 @@
-import { w as capitalize } from "./identifier-Blietev0.js";
-//#region node_modules/@mui/material/esm/utils/capitalize.js
-var capitalize_default = capitalize;
-//#endregion
-export { capitalize_default as t };
-
-//# sourceMappingURL=capitalize-eCJtuew8.js.map
