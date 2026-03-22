307c24e3ebc003e680ea51b0d62d6522e55c2da1
karensproject <karenitz2708@gmail.com>
2026-03-22 17:35:42 +0200

    edit .gitignore

--- Client/.vite/deps/memoTheme-CJUPF7oV.js
+++
@@ -1,7 +0,0 @@
-import { a as unstable_memoTheme } from "./identifier-Blietev0.js";
-//#region node_modules/@mui/material/esm/utils/memoTheme.js
-var memoTheme = unstable_memoTheme;
-//#endregion
-export { memoTheme as t };
-
-//# sourceMappingURL=memoTheme-CJUPF7oV.js.map
